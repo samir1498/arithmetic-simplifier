970789ad6492661c86b5bd0cb999a8e5f705f2bf
samir1498 <samirsbx2@gmail.com>
2023-04-30 13:03:33 +0100

    fixing merge conflict

--- main.js
+++ main.js
@@ -10,11 +10,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault()
   const input = InputValue.value
   const result = evaluateExpression(input)
-<<<<<<< HEAD
   ResultDiv.innerText = isNaN(result) ? "Invalid math expression" : result
-=======
-  ResultDiv.innerText = isNaN(result) ? "Invalid Math Expression" : result
->>>>>>> 30872c74b88f97c9cb48c0f52356d045322261a1
 })
 
 InputValue.addEventListener("input", (e) => {
